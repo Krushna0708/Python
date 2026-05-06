471da89e49b8b71d11569f28443f8b9c9096bae1
Krushna0708 <nkrushna135@gmail.com>
2026-05-06 13:18:00 +0530

    control statement(if-elif-else) practice problem

---
+++ control statement/if_elif_else/num_divisible_or_not.py
@@ -0,0 +1,8 @@
+# WAP to check  num is divisible by 9 or 6  or not.
+num  = int(input("Enter The  Any Number :"))
+
+# Using logical operation to perform this program.
+if num % 2 == 0 or num % 2 == 0:
+    print("The Number is Divisible By 9 or 6 ")
+else:
+    print("The Number is not Divisible by 9 or 6")
