6e23479fa06a452dad87ba0b0fdfe334cba75dec
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:04:17 +0530

    duplicate file deleted

--- control statement/if/palindrome.py
+++
@@ -1,8 +0,0 @@
-# WAP to check given string is palindrome or not.
- 
-name  = str(input("Enter Any String or Name :"))
-rev = name[::-1] #this line check string is palindrome or not.
-if name == rev:
-    print("The Given String is Palindrome ")
-else:
-    print("The Given string is Not Palindrome")
