16ab0f81859a102ddd4fa6c7fc4402bcb6f5d077
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:09:13 +0530

    delete duplicate file

--- control statement/if/check_num_is_float_dt.py
+++
@@ -1,6 +0,0 @@
-# WAP to check wether the given number is Float data type or not.
-num = eval(input("Enter Any Data Type  :"))
-if type(num) == float:
-    print("The Number is Float Data Type",type(num)) #type() check datatype
-else:
-    print("The Number is Not Float Data Type",type(num)) 
