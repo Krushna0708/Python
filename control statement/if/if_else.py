a81715dc05a878b1acea08b3567454cb71ec2207
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:06:14 +0530

    duplicate file deleted

--- control statement/if/if_else.py
+++
@@ -1,7 +0,0 @@
-# WAP to check the voter is eligible for vote or not.
-age = int(input("ENter Voter Age :"))
-
-if age >= 18:
-    print("The Voter is Eligible For Vote")
-else:
-    print("The Voter is Not Eligible For Vote")
