93c2969778b6dcdbabee391a209e68c9e1b0f293
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:09:42 +0530

    delete duplicate file

--- control statement/if/check_case.py
+++
@@ -1,8 +0,0 @@
-# WAP TO wether the given character is upper or lower case or special character
-char = input("Enter any Character : ")
-if 'a' <= char <= 'z': 
-    print("The Character is Lower case")
-elif 'A' <= char <= 'Z':
-    print("The Character is Upper case")
-else:
-    print("The Character is special Character")
