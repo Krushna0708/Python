f546a3fa3fad987308f5411644294f5a1d79d231
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:02:02 +0530

    Delete control statement/if/simple_if.py

--- control statement/if/simple_if.py
+++
@@ -1,6 +0,0 @@
-# WAP to check wether the digit is odd or not 
-a =  int(input("Enter any Digit :"))
-
-#This is logic to check digit is odd or not
-if a % 2 != 0:  # Another logic is : if a % 2 == 1:
-    print("The Digit is ODD")
