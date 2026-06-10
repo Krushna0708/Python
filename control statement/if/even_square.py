afbef4d4816632d53eb811712a556f79e2c827af
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:08:13 +0530

    delete control statement

--- control statement/if/even_square.py
+++
@@ -1,6 +0,0 @@
-# WAP to print the square of the number  only if it number is even
-a = int(input("Enter any Number :"))
-if a % 2 == 0:
-    print("The Number is Even and it square is :",a ** a)
-else:
-    print("The Number is odd")
