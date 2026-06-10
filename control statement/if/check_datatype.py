9a891ec2298a35e65bb699dbda9a1e35c7533000
Krushna_Nagare <nkrushna135@gmail.com>
2026-06-11 00:12:09 +0530

    delete directory

--- control statement/if/check_datatype.py
+++
@@ -1,8 +0,0 @@
-# WAP TO CHECK GIVEN NUMBER IS SINGLE VALUE DATA TYPE OR NOT.
-num = eval(input("Enter Any Data Type :"))
-
-# Using membership operator to solve this program.
-if num in [ int , float , complex , bool ]:
-    print("The Number of Data Type is Single Value Data Type ")
-else:
-    print("The Number of Data Type is Not Single Value Data Type , It is ",type(num))
