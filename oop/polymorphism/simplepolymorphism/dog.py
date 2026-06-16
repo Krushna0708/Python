eb913cfc8ebf14d8cb7f2a6bc45f7f989d262f92
Krushna0708 <nkrushna135@gmail.com>
2026-06-17 00:16:10 +0530

    Day 29 Polymorphism Practice Problem Completed

---
+++ oop/polymorphism/simplepolymorphism/dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    def sound(self):
+        print("Barking")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+c1 = Cat()
+c1.sound()
---
+++ oop/polymorphism/simplepolymorphism/laptop.py
@@ -0,0 +1,10 @@
+class Mobile:
+    def show(self):
+        print("Mobile")
+
+class Laptop:
+    def show(self):
+        print("Laptop")
+
+l1 = Laptop()
+l1.show()
